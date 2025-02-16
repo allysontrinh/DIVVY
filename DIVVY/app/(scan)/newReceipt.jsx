584b913a4e3f11fe3318f4899788d97066cf3b09
Sarah Jang <ssjang@umass.edu>
2025-02-16 08:40:32 -0500

    wer

--- DIVVY/app/(scan)/newReceipt.jsx
+++ DIVVY/app/(scan)/newReceipt.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { View, Text, Button, ScrollView } from "react-native";
 import { useRouter } from "expo-router";
 import { fetchFriends } from "../_utils/getFriends";
+import { postReceipt } from "../_utils/postReceipt";
 
 export default function newReceiptScreen({ veryfiData }) {
   const itemList = veryfiData.line_items;
   const router = useRouter();
+
+  const handlePost = async () => {
+    try {
+      const hi = await postReceipt(69, 69, 69, veryfiData);
+      console.log(hi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View
       style={{
@@ -16,13 +27,13 @@ export default function newReceiptScreen({ veryfiData }) {
       }}
     >
       <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 30 }}>
-        {verifyData.vendor.name}
+        {veryfiData.vendor.name}
       </Text>
 
       <ScrollView style={{ marginTop: 20, width: "100%" }}>
-        {itemList.map((item, index) => {
+        {/* {itemList.map((item, index) => {
           return (
-            <View>
+            <View key={index}>
               <Text style={{ marginBottom: 10 }}>Item: {item.description}</Text>
               <Text style={{ marginBottom: 10 }}>
                 Quantity: {item.quantity}
@@ -30,9 +41,9 @@ export default function newReceiptScreen({ veryfiData }) {
               <Text style={{ marginBottom: 10 }}>Price: {item.total}</Text>
             </View>
           );
-        })}
+        })} */}
       </ScrollView>
-      <Button title="Add Friends"></Button>
+      <Button title="Add Friends" onPress={() => handlePost()}></Button>
       <Button title="Go Back Home" onPress={() => router.push("/")} />
     </View>
   );
---
+++ DIVVY/app/_utils/postReceipt.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export const postReceipt = async (eventID, receiptID, userID, veryfiData) => {
+  const reqBody = {
+    receiptID: receiptID,
+    eventID: eventID,
+    totalPrice: veryfiData.total,
+    pricePerUser: 0,
+    priceRemaining: veryfiData.total,
+    users: [userID],
+    paid: [],
+    items: veryfiData.line_items.map((item, index) => ({
+      itemID: item.id,
+      totalPrice: item.total,
+      priceRemaining: item.total,
+      pricePerUser: item.total,
+    })),
+    history: [],
+  };
+
+  try {
+    const response = await axios.post(
+      "https://divvy-8y34.onrender.com/api/receipts",
+      reqBody
+    );
+    console.log("Receipt uploaded:", response.data);
+  } catch (error) {
+    console.error("Error posting receipt:", error);
+  }
+};
